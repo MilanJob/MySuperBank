2b4a909e45b01b813b7246e22e8927e81c22c283
Milan Ignjatovic <mignjatovic@real-ware.com>
2020-10-16 11:15:05 +0200

    add msUnitTest and add Nwetonsoft.Json package

---
+++ msUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using BankyStuffLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace msUnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestOverdraw()
+		{
+			var account = new BankAccount("Kendra", 10000);
+
+			// Test that the initial balances must be positive.
+			Assert.ThrowsException<InvalidOperationException>(
+				() => account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw")
+			);
+		}
+	}
+}
